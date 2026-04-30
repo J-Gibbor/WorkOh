f4c596f5aa733dfc6578b5ed595da22407fdc60a
John Gibbor Iro <johniro53@gmail.com>
2026-04-30 13:21:41 +0100

    memesticker fix

--- bot.mjs
+++ bot.mjs
@@ -1217,10 +1217,10 @@ memesticker: async () => {
 
   // ===== USE GENERATED TEXT ELEMENTS =====
   const svg = `
-  <svg width="512" height="512">
+ <svg width="512" height="512">
     <rect width="100%" height="100%" fill="white"/>
     <text x="50%" y="50%" font-size="40" text-anchor="middle" fill="black">
-      ${textElements}
+      ${text}
     </text>
   </svg>`
 
